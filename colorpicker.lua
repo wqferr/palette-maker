b7147c413c7430c0ab2c708c6f53d603bfd47c0d
wqferr <wqferr@gmail.com>
2016-08-11 11:08:03 -0300

    Create initial version of ColorPicker

--- colorpicker.lua
+++ colorpicker.lua
@@ -1,3 +1,53 @@
--- TODO create ColorPicker class that allows one to:
---  set cursor position
---  get the color of the pixel at the current position
+
+local ColorPicker = {}
+ColorPicker.__index = ColorPicker
+
+
+function ColorPicker.new(palette, cursorImg, cursorOffX, cursorOffY)
+    local cp = {}
+    setmetatable(cp, ColorPicker)
+
+    cp.palette = palette
+    cp.cursor = {
+        x = 0,
+        y = 0,
+        offX = cursorOffX or 0,
+        offY = cursorOffY or 0,
+        img = cursorImg
+    }
+
+    return cp
+end
+
+function ColorPicker:getCursorPos()
+    return self.cursor.x, self.cursor.y
+end
+
+function ColorPicker:setCursorPos(x, y)
+    if x then
+        self.cursor.x = x
+    end
+    if y then
+        self.cursor.y = y
+    end
+end
+
+function ColorPicker:getColor()
+    return self.palette:getData():getPixel(
+        self.cursor.x, self.cursor.y
+    )
+end
+
+function ColorPicker:draw(x, y)
+    love.graphics.draw(self.palette, x, y)
+    love.graphics.draw(self.cursor.img, x + self.cursor.x + self.cursor.offX, y + self.cursor.y + self.cursor.offY)
+end
+
+setmetatable(ColorPicker, {
+    __call = function(t, ...)
+        return ColorPicker.new(...)
+    end
+})
+
+
+return ColorPicker
--- main.lua
+++ main.lua
@@ -18,10 +18,21 @@ function love.load()
     gradient = love.graphics.newImage(gradientData)
     pickerCursorImg = love.graphics.newImage("img/cursor.png")
 
-    picker = ColorPicker(gradient, pickerCursorImg)
+    picker = ColorPicker(
+        gradient, pickerCursorImg,
+        -pickerCursorImg:getWidth()/2, -pickerCursorImg:getHeight()/8
+    )
 end
 
 function love.draw()
-
+    picker:draw(100, 100)
+    love.graphics.setColor(picker:getColor())
+    love.graphics.rectangle("fill", 500, 100, 50, 50)
+    love.graphics.setColor(255, 255, 255)
 end
 
+function love.mousemoved(x, y)
+    x = x - 100
+    x = math.max(0, math.min(x, gradient:getWidth() - 1))
+    picker:setCursorPos(x)
+end
