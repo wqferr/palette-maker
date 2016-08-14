4b4e2ed5903eeb13d57e6328c08345c8ca3662ab
wqferr <wqferr@gmail.com>
2016-08-14 09:34:37 -0300

    Integrate ColorPicker & ModeController to ClickMap

--- main.lua
+++ main.lua
@@ -9,6 +9,7 @@ local picker
 
 function love.load()
     gradientData = love.image.newImageData(200, 30)
+    pickerCM = ClickMap()
 
     gradientData:mapPixel(
         function(x, y, r, g, b, a)
@@ -48,21 +49,39 @@ function love.load()
         -pickerCursorImg:getWidth()/2,
         -pickerCursorImg:getHeight()/8
     )
+    picker.x, picker.y = 100, 100
+
+    local region = pickerCM:newRegion(
+        "rect",
+        function(r, x, y)
+            pickerController:setMode("grab", r.picker)
+        end,
+        function(r, x, y)
+            pickerController:setMode("normal")
+        end,
+        picker.x, picker.y, picker:getWidth(), picker:getHeight()
+    )
+    region.picker = picker
 end
 
 function love.draw()
-    picker:draw(100, 100)
+    picker:draw(picker.x, picker.y)
     love.graphics.setColor(picker:getColor())
     love.graphics.rectangle("fill", 500, 100, 50, 50)
     love.graphics.setColor(255, 255, 255)
 end
 
 function love.mousepressed(x, y, mb)
-    local r = clickmap:click(x, y)
+    local r = pickerCM:click(x, y)
+    pickerController.updateCursor(x - picker.x)
+end
+
+function love.mousereleased(x, y, mb)
+    pickerCM:release(x, y)
 end
 
 function love.mousemoved(x, y)
-    x = x - 100
+    x = x - picker.x
     x = math.max(0, math.min(x, gradient:getWidth() - 1))
     pickerController.updateCursor(x)
 end
