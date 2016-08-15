821ebe77e7079f3291cffea23cb341c74ec28111
wqferr <wqferr@gmail.com>
2016-08-15 00:24:23 -0300

    Add config file

---
+++ conf.lua
@@ -0,0 +1,17 @@
+function love.conf(t)
+    t.version = "0.10.1"
+    t.window.title = "PaletteMaker"
+
+    t.window.width = 800
+    t.window.height = 680
+
+    t.modules.audio = false
+    t.modules.joystick = false
+    t.modules.math = false
+    t.modules.physics = false
+    t.modules.sound = false
+    t.modules.timer = false
+    t.modules.touch = false
+    t.modules.video = false
+    t.modules.thread = false
+end
