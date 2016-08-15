262d4eb60b0a27ced318728ebf61d17094ceb1d3
wqferr <wqferr@gmail.com>
2016-08-15 00:52:42 -0300

    Remove window resize from config file

--- conf.lua
+++ conf.lua
@@ -2,9 +2,6 @@ function love.conf(t)
     t.version = "0.10.1"
     t.window.title = "PaletteMaker"
 
-    t.window.width = 800
-    t.window.height = 680
-
     t.modules.audio = false
     t.modules.joystick = false
     t.modules.math = false
